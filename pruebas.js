20e13a5cc8bb1d517d76cd11b6467ac697ccd1f0
Aitor <aitordonado@gmail.com>
2023-06-07 10:49:01 +0200

    Modificado pruebas.js

---
+++ pruebas.js
@@ -0,0 +1,9 @@
+use videoclubDB
+
+db.createCollection('usuarios')
+
+db.usuarios.insertOne({"Nombre" : "Pedro", "Apellido" : "López"})
+usuario2 = {"Nombre" : "Luis", "Apellido" : "Gómez"}
+db.usuarios.insertOne(usuario2)
+
+db.usuarios.find()
