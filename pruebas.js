3bfa67cd808f3158e236dbc915c169a7fa6b3948
Txomin <t.uriartesaiatuz2023@gmail.com>
2023-06-07 14:14:55 +0200

    filtrado jubilados

--- pruebas.js
+++ pruebas.js
@@ -65,3 +65,6 @@ db.usuarios.updateOne({$and: [{ Edad: { $gte: 20 }},{Apellido: "López"}]}, {$se
 
 
 db.usuarios.find({Ciudad: {$in:["San Sebastián", "Irún"]}})
+
+db.usuarios.updateMany({ Edad: { $gte: 65 }}, {$set: {Profesion: "Jubilado"}})
+
