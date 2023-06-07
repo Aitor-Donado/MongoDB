26f686d8e25f54e186caff9ddd40dc71c1ae68af
Aitor <aitordonado@gmail.com>
2023-06-07 12:07:05 +0200

    commit

--- pruebas.js
+++ pruebas.js
@@ -9,3 +9,12 @@ db.usuarios.insertOne(usuario2)
 db.usuarios.find()
 
 db.usuarios.insertMany([{"Nombre": "Txomin","Apellido": "López"},{"Nombre": "Anabel","Apellido": "Gómez"},{"Nombre": "Denis","Apellido": "Almandoz"}, {"Nombre": "David","Apellido": "Tobias"}])
+
+
+db.usuarios.find({Nombre:"Anabel"})
+db.usuarios.find({Apellido: "Gómez"})
+
+db.usuarios.findOne({Apellido: "Gómez"})
+
+db.usuarios.find({Apellido: "Gómez"}).count()
+
