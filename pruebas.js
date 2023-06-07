ee2a657328c0094ec87345da11933a233cb08bd3
Zack20100 <131264480+Zack20100@users.noreply.github.com>
2023-06-07 12:41:38 +0200

    save

--- pruebas.js
+++ pruebas.js
@@ -9,3 +9,4 @@ db.usuarios.insertOne(usuario2)
 db.usuarios.find()
 
 db.usuarios.insertMany([{"Nombre": "Txomin","Apellido": "López"},{"Nombre": "Anabel","Apellido": "Gómez"},{"Nombre": "Denis","Apellido": "Almandoz"}, {"Nombre": "David","Apellido": "Tobias"}])
+
