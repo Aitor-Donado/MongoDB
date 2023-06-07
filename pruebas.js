fa23b57ea2e868a0fe5c34065833295960584644
Aitor <aitordonado@gmail.com>
2023-06-07 12:47:34 +0200

    comm

--- pruebas.js
+++ pruebas.js
@@ -10,8 +10,6 @@ db.usuarios.find()
 
 db.usuarios.insertMany([{"Nombre": "Txomin","Apellido": "López"},{"Nombre": "Anabel","Apellido": "Gómez"},{"Nombre": "Denis","Apellido": "Almandoz"}, {"Nombre": "David","Apellido": "Tobias"}])
 
-<<<<<<< HEAD
-=======
 
 db.usuarios.find({Nombre:"Anabel"})
 db.usuarios.find({Apellido: "Gómez"})
