00eaf13d8b464c3e3911b6c1ed9be10983e4b801
Aitor <aitordonado@gmail.com>
2023-06-07 14:04:32 +0200

    comm

--- pruebas.js
+++ pruebas.js
@@ -65,3 +65,9 @@ db.usuarios.updateOne({$and: [{ Edad: { $gte: 20 }},{Apellido: "López"}]}, {$se
 
 
 db.usuarios.find({Ciudad: {$in:["San Sebastián", "Irún"]}})
+
+
+db.usuarios.find({Profesion: {$exists: false}})
+
+db.usuarios.updateMany({Profesion: {$exists: false}},{$set: {Profesion: "Parado"}})
+
